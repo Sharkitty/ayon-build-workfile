9ebfc7922c7081c4b93e69300b69166a2c2a5194
Sharkitty <hayleylalicorne@gmail.com>
2025-04-01 16:27:28 +0200

    wip settings

--- server/settings/main.py
+++ server/settings/main.py
@@ -1,18 +1,36 @@
-from ayon_server.settings import BaseSettingsModel
+from ayon_server.settings import BaseSettingsModel, SettingsField
 
 
 class DefaultSettingsModel(BaseSettingsModel):
-    pass
+    execute_synchronously: bool = SettingsField(
+        False, title="Execute Synchronously"
+    )
+    loader_regex: rstring = SettingsField(r"", title="Loader")
+    hero_version: bool = SettingsField(False, title="Hero Version")
 
 
 class SettingsExceptionsModel(BaseSettingsModel):
-    pass
+    entity_names: list[rstring] = SettingsField(
+        title="Entity Names", default_factory=list
+    )
+    task_types: list[rstring] = SettingsField(
+        title="Task Types", default_factory=list
+    )
+    use_other_task_workfile: bool = SettingsField(
+        False, title="Use Other Task Workfile"
+    )
+    # TODO Should appear only if `use_other_task_workfile` is set to True
+    other_task: string = SettingsField("", title="Other Task")
 
 
 class BuildWorkfileSettings(BaseSettingsModel):
-    default_settings: DefaultSettingsModel = None
+    default_settings: DefaultSettingsModel = SettingsField(
+        title="Default Settings", default_factory=DefaultSettingsModel
+    )
 
-    settings_exceptions: SettingsExceptionsModel = None
+    settings_exceptions: list[SettingsExceptionsModel] = SettingsField(
+        title="Settings Exceptions", default_factory=list
+    )
 
 
 DEFAULT_VALUES = {
