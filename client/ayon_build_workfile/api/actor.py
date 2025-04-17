4d0e127b4575d8df9b0f4b4abd86ef80aa7f1545
Sharkitty <hayleylalicorne@gmail.com>
2025-04-17 17:25:22 +0200

    en get_casting, add product ref to actor class

--- client/ayon_build_workfile/api/actor.py
+++ client/ayon_build_workfile/api/actor.py
@@ -6,31 +6,39 @@ class Actor:
     # Or loader class?
     loader: string = None
     # type: reference to a product
-    product_to_switch = None
+    product_reference = None
 
     def __init__(
-        kitsu_actor: dict,
+        actor: dict,
         server_settings: dict = None,
         settings_overrides: dict = None,
+        product_reference = None,
     ):
-        # TODO get product_name and product_type from kitsu_actor
+        self.product_name = actor.get("name")
+        # TODO product_type from kitsu_actor
 
         main_settings = get_main_settings(server_settings)
         settings_exceptions = get_settings_exceptions(server_settings)
 
+        self.product_reference = product_reference
+
     def __init__(
-        kitsu_actor: dict,
+        actor: dict,
         main_settings: dict,
         settings_exceptions: dict,
         settings_overrides: dict = None,
+        product_reference = None,
     ):
-        pass
+        self.product_name = actor.get("name")
+
+        self.product_reference = product_reference
 
     def __init__(
         product_name: string,
         product_type: string,
         server_settings: dict = None,
         settings_overrides: dict = None,
+        product_reference = None,
     ):
         self.product_name = product_name
         self.product_type = product_type
@@ -38,15 +46,20 @@ class Actor:
         main_settings = get_main_settings(server_settings)
         settings_exceptions = get_settings_exceptions(server_settings)
 
+        self.product_reference = product_reference
+
     def __init__(
         product_name: string,
         product_type: string,
         main_settings: dict,
         settings_exceptions: dict,
         settings_overrides: dict = None,
+        product_reference = None,
     ):
         self.product_name = product_name
         self.product_type = product_type
+
+        self.product_reference = product_reference
 
     def build():
         pass
--- client/ayon_build_workfile/api/casting_manager.py
+++ client/ayon_build_workfile/api/casting_manager.py
@@ -1,5 +1,8 @@
 import gazu
 
+from .actor import Actor
+from .server_settings import get_server_settings, get_settings_overrides
+
 
 def get_casting(asset_name: str) -> dict:
     # TODO get entity id
@@ -12,9 +15,27 @@ def get_casting(asset_name: str) -> dict:
     # TODO get entity type
     entity_type = "asset"
 
+    casting = None
     if entity_type == "asset":
-        return gazu.casting.get_asset_casting(entity)
+        casting = gazu.casting.get_asset_casting(entity)
     elif entity_type == "shot":
-        return gazu.casting.get_shot_casting(entity)
+        casting = gazu.casting.get_shot_casting(entity)
     else:
         raise RuntimeError(f"{entity_type} is not a recognized entity type.")
+
+    assert casting, "Casting not found."
+
+    actors = []
+    server_settings = get_server_settings()
+    settings_overrides = get_settings_overrides()
+    for actor in casting:
+        product_reference = None
+
+        # TODO find if actor exists in scene
+
+        # TODO create new Actor object and append it to `actors`
+        actors.append(
+            Actor(actor, server_settings, settings_overrides)
+        )
+
+    return actors
