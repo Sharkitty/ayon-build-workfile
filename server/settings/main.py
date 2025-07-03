3858a04dd31375d364c07390060c967d3eb802ab
normaal <normaal@macmini.home>
2025-07-03 17:11:27 +0200

    fix settings

--- server/settings/main.py
+++ server/settings/main.py
@@ -5,7 +5,7 @@ class MainSettingsModel(BaseSettingsModel):
     execute_synchronously: bool = SettingsField(
         False, title="Execute Synchronously"
     )
-    loader_regex: str = SettingsField(r"", title="Loader")
+    loader_regex: str = SettingsField("", title="Loader")
     hero_version: bool = SettingsField(False, title="Hero Version")
 
 
@@ -16,7 +16,7 @@ class SettingsExceptionsModel(BaseSettingsModel):
     task_types: list[str] = SettingsField(
         title="Task Types", default_factory=list
     )
-    loader_regex: str = SettingsField(r"", title="Loader")
+    loader_regex: str = SettingsField("", title="Loader")
     use_other_task_workfile: bool = SettingsField(
         False, title="Use Other Task Workfile"
     )
@@ -37,14 +37,8 @@ class BuildWorkfileSettings(BaseSettingsModel):
 DEFAULT_VALUES = {
     "main_settings": {
         "execute_synchronously": False,
-        "loader_regex": r"",
+        "loader_regex": "",
         "hero_version": False,
     },
-    "settings_exceptions": {
-        "entity_names": [],
-        "task_types": [],
-        "loader_regex": r"",
-        "use_other_task_workfile": False,
-        "other_task": "",
-    },
+    "settings_exceptions": [],
 }
